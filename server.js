4f993bf0d5e6d0cdb5f604ee1efd79a80d81dd71
PauloStraforini <paulostraforini@gmail.com>
2024-12-03 20:13:06 -0300

    ✨ Update: server.js

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 import express from 'express';
 
-const app = express();
+const app = express()
+app.use(express.json())
 
-app
+const users = []
+
+app.post ('/usuarios', (req, res) => {
+
+    users.push(req.body)
+
+    res.send("Salve Parça")
+})
+
+app.get('/usuarios', (req, res) => {
+    res.json(users)
+})
+
+app.listen(2001)
