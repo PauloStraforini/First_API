61fb4df26a2df7d1d2c3e2ba71d7eaaf2d510e19
PauloStraforini <paulostraforini@gmail.com>
2024-12-03 20:27:29 -0300

    ✨ Update: server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
@@ -7,7 +10,17 @@ const users = []
 
 app.post ('/usuarios', (req, res) => {
 
-    users.push(req.body)
+    prisma.user.create({
+        data: {
+            name: req.body.name,
+            numberuser: req.body.numberuser
+        }
+    }).then(user => {
+        res.json(user)
+    }).catch(err => {
+        res.json({error: 'Ocorreu um erro ao criar o usuário'})
+    })
+
 
     res.send("Salve Parça")
 })
